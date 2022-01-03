532692c8b62fc14ee7a26682940ebee7913bf9ff
Sergei Shapovalov <1serhio46@gmail.com>
2022-01-03 17:05:20 +0300

    fix: try to fix paths error

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import userController from '../controllers/user.controller';
 import Router from 'express';
-
+//import userController from 'controllers/user.controller'; //не понимаю почему при таком импорте пишет cant find module controllers/user.controller, хотя в user.model так импортирую module.interface и все ок
+//import userController from '@controllers/user.controller'; // и так тож не работает, не могу допереть что не так)
 const router = Router();
 
 //create user
